5236a475b4cfe68ba58a6f4b06c02d3aadbb52c3
Motaphe <suzalregmii@gmail.com>
2023-07-18 21:38:23 -0400

    updated polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# For HWMON Path
+path1="/sys/devices/platform/coretemp.0/hwmon/hwmon5/temp1_input"
+path2="/sys/devices/platform/coretemp.0/hwmon/hwmon6/temp1_input"
+
+if [[ -f $path1 ]]; then
+    export HWMON_PATH="$path1"
+elif [[ -f $path2 ]]; then
+    export HWMON_PATH="$path2"
+else
+    echo "Error: Temperature file not found."
+fi
+
+
+# Launch Polybar
 if [ -z "$(pgrep -x polybar)" ]; then
     BAR="main"
     for m in $(polybar --list-monitors | cut -d":" -f1); do
