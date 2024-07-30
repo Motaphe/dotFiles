b7f0a48dadcfd15e4774088ce71a2445de32c420
Motaphe <suzalregmii@gmail.com>
2024-07-30 01:16:41 -0400

    fg_urgent was too red

--- .config/awesome/themes/custom/theme.lua
+++ .config/awesome/themes/custom/theme.lua
@@ -21,7 +21,7 @@ theme.bg_systray = theme.bg_normal
 
 theme.fg_normal = "#aaaaaa"
 theme.fg_focus = "#ffffff"
-theme.fg_urgent = "#cf1b1b"
+theme.fg_urgent = "#ffffff"
 theme.fg_minimize = "#ffffff"
 
 theme.useless_gap = dpi(0)
